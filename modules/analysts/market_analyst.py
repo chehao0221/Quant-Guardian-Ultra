2fab4ad6766d2551e7abde4903eb2d0c61dcd7f8
chehao0221 <123921114+chehao0221@users.noreply.github.com>
2025-12-26 13:53:24 +0800

    Update market_analyst.py

--- modules/analysts/market_analyst.py
+++ modules/analysts/market_analyst.py
@@ -2,9 +2,8 @@ from .base_analyst import BaseAnalyst
 
 class MarketAnalyst(BaseAnalyst):
     def __init__(self, market):
-        # 初始化父類別 (TW)
         super().__init__(market)
 
     def analyze(self, symbol):
-        # 呼叫 BaseAnalyst 裡的 predict 邏輯
+        # 直接調用 base_analyst.py 裡的 predict 邏輯
         return self.predict(symbol)
